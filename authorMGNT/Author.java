560fe3954dfa92ca80b7c527ad0477bd6129bc0e
Declan <declan.derible@keyin.com>
2024-10-27 22:16:32 -0230

    Item & Author Managment groundwork

--- authorMGNT/Author.java
+++ authorMGNT/Author.java
@@ -1,5 +1,51 @@
 package java_midterm.authorMGNT;
-
+import java.util.ArrayList;
+import java.util.List;
+import itemMGNT.LibraryItem;
 public class Author {
-    
+    private String name;
+    private String dateOfBirth;
+    private List<LibraryItem> writtenItems;
+
+    public Author(String name, String dateOfBirth){
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.writtenItems = new ArrayList<>();
+    }
+
+    public void addWrittenItem(LibraryItem item){
+        writtenItems.add(item);
+    }
+
+    public void removeWrittenItem(LibraryItem item){
+        writtenItems.remove(item);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDateOfBirth(){
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<LibraryItem> getWrittenItems(){
+        return writtenItems;
+    }
+
+    public String toString(){
+        return "Author{" +
+        "Name ='" + name + '\'' +
+        ", Date Of Birth ='" + dateOfBirth + '\'' +
+        ", Books Written =" + writtenItems +
+        '}';
+    }
 }
--- itemMGNT/LibraryItem.java
+++ itemMGNT/LibraryItem.java
@@ -1,5 +1,80 @@
 package java_midterm.itemMGNT;
 
-public class LibraryItem {
+public abstract class LibraryItem {
+    private String id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String publisher;
+    private int copies;
+
+    public LibraryItem(String id, String title, String author, String isbn, String publisher, int copies){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.copies = copies;
+    }
+
+    public abstract String getItemType();
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
     
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
+
+    public int getCopies(){
+        return copies;
+    }
+
+    public void setCopies(int copies){
+        this.copies = copies;
+    }
+
+    public String toString(){
+        return "LibraryItem{" +
+                "ID='" + id + '\'' +
+                ", Title ='" + title + '\'' +
+                ", Author ='" + author + '\'' +
+                ", ISBN ='" + isbn + '\'' +
+                ", Publisher ='" + publisher + '\'' +
+                ", Copies =" + copies +
+                '}';
+    }
 }
